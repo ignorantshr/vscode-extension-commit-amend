8bf977e072549f5dec64ee4ee676abc76b276aed
ignorantshr <shrshraa@outlook.com>
2025-01-23 20:27:13 +0800

    close terminal automatically

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,5 @@
 import * as vscode from 'vscode';
 
-let terminal: vscode.Terminal | undefined;
-
 export function activate(context: vscode.ExtensionContext) {
     const disposable = vscode.commands.registerCommand('extension.gitCommitAmend', () => {
         amendLastCommit();
@@ -13,26 +11,18 @@ export function activate(context: vscode.ExtensionContext) {
     button.show();
 
     context.subscriptions.push(disposable, button);
-
-    vscode.window.onDidCloseTerminal((closedTerminal: vscode.Terminal) => {
-        if (closedTerminal === terminal) {
-            handleTerminalClosed();
-        }
-    });
 }
 
 async function amendLastCommit() {
     try {
-        terminal = vscode.window.createTerminal();
+        let terminal = vscode.window.createTerminal({ hideFromUser: true });
         terminal.sendText('git commit --amend --no-edit');
+        setTimeout(() => {
+            terminal.dispose();
+        }, 2000);
     } catch (error) {
         vscode.window.showErrorMessage('Failed to amend git commit:', error.message);
     }
-}
-
-function handleTerminalClosed() {
-    terminal = undefined;
-    // vscode.window.showInformationMessage('Git commit amended successfully!');
 }
 
 export function deactivate() { }
